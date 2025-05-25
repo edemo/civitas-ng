b0873062ac1f8c82f0021638145650a5c56ec8ad
Arpad Magosanyi <m4gw4s@gmail.com>
2025-05-25 16:08:50 +0200

    FIXUP

--- src/test/java/civitas/crypto/concrete/CryptoAlgsTest.java
+++
@@ -1,8 +0,0 @@
-package civitas.crypto.concrete;
-
-import civitas.crypto.algorithms.Constants;
-
-public class CryptoAlgsTest extends ConcreteTestBase
-		implements ConcreteTestData, Constants {
-
-}
