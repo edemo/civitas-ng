119b3c24e751f7b46ba917980b2f77ee652a984d
gbq6 <24852447+gbq6@users.noreply.github.com.>
2025-09-17 18:29:16 +0200

    Fixed array copy

--- civitas.common/src/main/java/civitas/common/mix/capabilitymix/AddEncryptedCapability.java
+++ civitas.common/src/main/java/civitas/common/mix/capabilitymix/AddEncryptedCapability.java
@@ -9,7 +9,7 @@ public class AddEncryptedCapability {
 	public void apply(CapabilityMix that, ElGamalCiphertextish v) {
 		ElGamalCiphertextish[] caps = that.capabilities;
 
-		ElGamalCiphertextish[] n = Arrays.copyOf(caps, caps.length + 1);
+		ElGamalCiphertextish[] n = Arrays.copyOf(caps, caps.length + 1, ElGamalCiphertextish[].class);
 		n[caps.length] = v;
 
 		that.capabilities = n;
